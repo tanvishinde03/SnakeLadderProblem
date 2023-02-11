264a17e1a6752690a06d0a4716685c3efcf6109a
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-11 17:50:31 +0530

    [Tanvi] Add : UC3 - Check for the Options

--- SnakeLadderProblem/SnakeLadderGame.cs
+++ SnakeLadderProblem/SnakeLadderGame.cs
@@ -17,10 +17,38 @@ namespace SnakeLadderProblem
 
             Random random = new Random();
             int DiceRoll = random.Next(1, 7);
-            Console.WriteLine("Dice Number : " + DiceRoll);
+            Console.WriteLine("Dice Number : " + DiceRoll);             //UC2
             Console.ReadLine();
 
+            Random random1 = new Random();
+            int option = random1.Next(1, 4);
+            Console.WriteLine("Number of option " + option);           //UC3
+            Console.Write("\n");
 
+            int Player_position = 0;
+            int No_Play = 0;
+
+            if (option == 1)
+            {
+                Player_position = Player_position + DiceRoll;
+                Console.WriteLine("Player position moves ahead by the number on dice : " + Player_position);
+                Console.ReadLine();
+            }
+            else if (option == 2)
+            {
+                Player_position = Player_position - DiceRoll;
+                Console.WriteLine("Player position moves behind by the number on dice : " + Player_position);
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Player_position = Player_position + No_Play;
+                Console.WriteLine("Player position remain same : " + Player_position);
+                Console.ReadLine();
+            }
+
+            
         }
 
 
