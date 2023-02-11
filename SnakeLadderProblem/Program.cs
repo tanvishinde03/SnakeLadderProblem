77879f75c3679017c2d6d8dda5c9bff6da8966e1
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-11 17:32:33 +0530

    [Tanvi] Add : UC1 - Single Player Position

---
+++ SnakeLadderProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public class Program
+    {
+         public static void Main(string[] args)
+        {
+
+            SnakeLadderGame position = new SnakeLadderGame();
+            position.PoisitionCheck();
+        }
+
+
+
+    }
+    }
+
---
+++ SnakeLadderProblem/SnakeLadderGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderProblem
+{
+    public class SnakeLadderGame
+    {
+        public void PoisitionCheck()
+        {
+            int player_no = 1;
+            int start_position = 0;
+            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no, start_position);
+            Console.ReadLine();
+        }
+
+
+
+
+
+    }
+}
